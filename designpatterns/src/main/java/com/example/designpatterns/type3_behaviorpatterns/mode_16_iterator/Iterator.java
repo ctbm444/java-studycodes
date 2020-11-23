a0bc58ff98fc496a90e765eabda1f0e6bc302d43
wsxiaotiana <wsxiaotiana@163.com>
2020-11-23 19:24:20 +0800

    添加迭代器模式示例

---
+++ designpatterns/src/main/java/com/example/designpatterns/type3_behaviorpatterns/mode_16_iterator/Iterator.java
@@ -0,0 +1,12 @@
+package com.example.designpatterns.type3_behaviorpatterns.mode_16_iterator;
+
+/**
+ * @description: 抽象迭代器
+ * @author: yanxiaotian
+ * @create: 2020-11-23 18:39
+ **/
+public interface Iterator {
+    Object first();
+    Object next();
+    boolean hasNext();
+}
